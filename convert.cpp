b6145cb7a69c82c55ec92d51626cec5580a8bbf1
Garrett Reid <cgreid1412@gmail.com>
2025-11-23 15:16:13 -0600

    Added some arguments.

--- convert.cpp
+++ convert.cpp
@@ -1,52 +1,43 @@
-//#include <cstddef>
+// #include <cstddef>
+#include <cctype>
 #include <exception>
 #include <functional>
 #include <iostream>
-#include <unordered_map>
-#include <string>
 #include <stdexcept>
-#include <cctype>
+#include <string>
+#include <unordered_map>
 
+const double _version{0.5};
 
-std::unordered_map<std::string, double> length_factors {
-    {"m", 1.0},
-    {"cm", 0.01},
-    {"mm", 0.001},
-    {"ft", 0.3048},
-    {"yd", 0.9144},
-    {"km", 1000},
-    {"mi", 1609.34}
-};
+std::unordered_map<std::string, double> length_factors{
+    {"m", 1.0},     {"cm", 0.01}, {"mm", 0.001},  {"ft", 0.3048},
+    {"yd", 0.9144}, {"km", 1000}, {"mi", 1609.34}};
 
-std::unordered_map<std::string, double> mass_factors {
-    {"kg", 1.0},
-    {"g", 0.001},
-    {"lb", 0.453592},
-    {"oz", 0.0283495}
-};
+std::unordered_map<std::string, double> mass_factors{
+    {"kg", 1.0}, {"g", 0.001}, {"lb", 0.453592}, {"oz", 0.0283495}};
 
-std::unordered_map<std::string, double> volume_factors {
-    {"L",      1.0},          // liter
-    {"l",      1.0},          // lowercase alias
-    {"mL",     0.001},        // milliliter
-    {"ml",     0.001},        // lowercase alias
-    {"uL",     0.000001},     // microliter
-    {"ul",     0.000001},     // lowercase alias
+std::unordered_map<std::string, double> volume_factors{
+    {"L", 1.0},       // liter
+    {"l", 1.0},       // lowercase alias
+    {"mL", 0.001},    // milliliter
+    {"ml", 0.001},    // lowercase alias
+    {"uL", 0.000001}, // microliter
+    {"ul", 0.000001}, // lowercase alias
 
-    {"gal",    3.78541},      // US gallon
-    {"qt",     0.946353},     // US quart
-    {"pt",     0.473176},     // US pint
-    {"cup",    0.24},         // metric cup
-    {"floz",   0.0295735},    // US fluid ounce
+    {"gal", 3.78541},    // US gallon
+    {"qt", 0.946353},    // US quart
+    {"pt", 0.473176},    // US pint
+    {"cup", 0.24},       // metric cup
+    {"floz", 0.0295735}, // US fluid ounce
 
-    {"tbsp",   0.0147868},    // tablespoon
-    {"tsp",    0.00492892},   // teaspoon
+    {"tbsp", 0.0147868}, // tablespoon
+    {"tsp", 0.00492892}, // teaspoon
 
-    {"m3",     1000.0},       // cubic meter
-    {"cm3",    0.001},        // cubic centimeter = milliliter
-    {"cc",     0.001},        // cc (same as mL)
-    {"in3",    0.0163871},    // cubic inch
-    {"ft3",    28.3168},      // cubic foot
+    {"m3", 1000.0},     // cubic meter
+    {"cm3", 0.001},     // cubic centimeter = milliliter
+    {"cc", 0.001},      // cc (same as mL)
+    {"in3", 0.0163871}, // cubic inch
+    {"ft3", 28.3168},   // cubic foot
 };
 
 struct TempUnit {
@@ -54,25 +45,19 @@ struct TempUnit {
     std::function<double(double)> from_celsius;
 };
 
-std::unordered_map<std::string, TempUnit> temp_units {
+std::unordered_map<std::string, TempUnit> temp_units{
     // Celsius
-    {"C", TempUnit {
-        [](double c) { return c; },
-        [](double c) { return c; }
-    }},
+    {"C", TempUnit{[](double c) { return c; }, [](double c) { return c; }}},
     // Fahrenheit
-    {"F", TempUnit {
-        [](double f) { return (f - 32.0) * 5.0 / 9.0; },
-        [](double c) { return c * 9.0 / 5.0 + 32.0; }
-    }},
+    {"F", TempUnit{[](double f) { return (f - 32.0) * 5.0 / 9.0; },
+                   [](double c) { return c * 9.0 / 5.0 + 32.0; }}},
     // Kelvin
-    {"K", TempUnit {
-        [](double k) { return k - 273.15; },
-        [](double c) { return c + 273.15; }
-    }},
+    {"K", TempUnit{[](double k) { return k - 273.15; },
+                   [](double c) { return c + 273.15; }}},
 };
 
-// std::unordered_map<std::string, double> currency_factors {   <-- This is a WIP
+// std::unordered_map<std::string, double> currency_factors {   <-- This is a
+// WIP
 //     { "USD", 1.0 },
 //     { "EUR", 1.1 },
 //     { "GBP", 1.3 },
@@ -84,39 +69,64 @@ std::unordered_map<std::string, TempUnit> temp_units {
 // Maps "weird user input" -> canonical unit key used in your factor maps
 std::unordered_map<std::string, std::string> unit_aliases = {
     // length
-    {"meter", "m"}, {"meters", "m"},
-    {"metre", "m"}, {"metres", "m"},
-    {"kilometer", "km"}, {"kilometers", "km"},
-    {"kilometre", "km"}, {"kilometres", "km"},
-    {"foot", "ft"}, {"feet", "ft"},
-    {"yard", "yd"}, {"yards", "yd"},
-    {"mile", "mi"}, {"miles", "mi"},
+    {"meter", "m"},
+    {"meters", "m"},
+    {"metre", "m"},
+    {"metres", "m"},
+    {"kilometer", "km"},
+    {"kilometers", "km"},
+    {"kilometre", "km"},
+    {"kilometres", "km"},
+    {"foot", "ft"},
+    {"feet", "ft"},
+    {"yard", "yd"},
+    {"yards", "yd"},
+    {"mile", "mi"},
+    {"miles", "mi"},
 
     // mass
-    {"kilogram", "kg"}, {"kilograms", "kg"},
-    {"gram", "g"}, {"grams", "g"},
-    {"pound", "lb"}, {"pounds", "lb"},
-    {"lbs", "lb"},    // common typo / plural
-    {"ounce", "oz"}, {"ounces", "oz"},
+    {"kilogram", "kg"},
+    {"kilograms", "kg"},
+    {"gram", "g"},
+    {"grams", "g"},
+    {"pound", "lb"},
+    {"pounds", "lb"},
+    {"lbs", "lb"}, // common typo / plural
+    {"ounce", "oz"},
+    {"ounces", "oz"},
 
     // volume
-    {"liter", "L"}, {"liters", "L"},
-    {"litre", "L"}, {"litres", "L"},
-    {"milliliter", "mL"}, {"milliliters", "mL"},
-    {"millilitre", "mL"}, {"millilitres", "mL"},
-    {"cup", "cup"}, {"cups", "cup"},
-    {"tablespoon", "tbsp"}, {"tablespoons", "tbsp"},
-    {"teaspoon", "tsp"}, {"teaspoons", "tsp"},
+    {"liter", "L"},
+    {"liters", "L"},
+    {"litre", "L"},
+    {"litres", "L"},
+    {"milliliter", "mL"},
+    {"milliliters", "mL"},
+    {"millilitre", "mL"},
+    {"millilitres", "mL"},
+    {"cup", "cup"},
+    {"cups", "cup"},
+    {"tablespoon", "tbsp"},
+    {"tablespoons", "tbsp"},
+    {"teaspoon", "tsp"},
+    {"teaspoons", "tsp"},
 
     // temperature
-    {"c", "C"}, {"celsius", "C"}, {"centigrade", "C"},
-    {"f", "F"}, {"fahrenheit", "F"},
-    {"k", "K"}, {"kelvin", "K"},
+    {"c", "C"},
+    {"celsius", "C"},
+    {"centigrade", "C"},
+    {"f", "F"},
+    {"fahrenheit", "F"},
+    {"k", "K"},
+    {"kelvin", "K"},
 };
 
-
 void print_usage() {
-    std::cout << "Usage: convert -f (From Unit) -t (To Unit) <num>" << std::endl; 
+    std::cout << "Usage: convert -f <from_unit> -t <to_unit> <value>\n"
+                 "Options:\n"
+                 "  -h, --help        Show this help message\n"
+                 "  -l, --list        List supported units\n"
+              << std::endl;
 }
 
 struct ConversionRule {
@@ -125,14 +135,13 @@ struct ConversionRule {
     std::function<double(double)> convert;
 };
 
-
-
 struct Args {
     std::string from_unit;
     std::string to_unit;
     double value;
-    bool show_help {false};
-    bool list_units {false};
+    bool show_help{false};
+    bool list_units{false};
+    bool show_version{false};
 };
 
 std::string to_lower(std::string s) {
@@ -154,32 +163,35 @@ std::string normalize_unit(std::string u) {
     return u;
 }
 
-Args parse_args(int argc, char* argv[]) {
+Args parse_args(int argc, char *argv[]) {
     // Argument handling
     Args result;
-    bool have_from {false};
-    bool have_to {false};
-    bool have_value {false};
+    bool have_from{false};
+    bool have_to{false};
+    bool have_value{false};
 
     for (int i = 1; i < argc; ++i) {
-        
+
         std::string arg = argv[i];
 
         if (arg == "-h" || arg == "--help") {
             result.show_help = true;
         } else if (arg == "-l" || arg == "--list" || arg == "--units") {
             result.list_units = true;
+        } else if (arg == "-v" || arg == "--version") {
+            result.show_version = true;
         } else if (arg == "-f" || arg == "--from") {
             if (i + 1 < argc) {
-                if (i+1 >= argc) {
-                    throw std::runtime_error("'-f/--from' flag requires a unit.");
+                if (i + 1 >= argc) {
+                    throw std::runtime_error(
+                        "'-f/--from' flag requires a unit.");
                 }
                 result.from_unit = normalize_unit(argv[++i]);
                 have_from = true;
             }
         } else if (arg == "-t" || arg == "--to") {
             if (i + 1 < argc) {
-                if (i+1 >= argc) {
+                if (i + 1 >= argc) {
                     throw std::runtime_error("'-t/--to' flag requires a unit.");
                 }
                 result.to_unit = normalize_unit(argv[++i]);
@@ -189,9 +201,8 @@ Args parse_args(int argc, char* argv[]) {
             try {
                 result.value = std::stod(arg);
                 have_value = true;
-        
-            }
-            catch (const std::exception&) {
+
+            } catch (const std::exception &) {
                 throw std::invalid_argument("Value must be a valid number.");
             }
         }
@@ -205,7 +216,6 @@ Args parse_args(int argc, char* argv[]) {
 
     return result;
 }
-
 
 double convert(std::string from_unit, std::string to_unit, double value) {
 
@@ -223,44 +233,42 @@ double convert(std::string from_unit, std::string to_unit, double value) {
 
     if (length_factors.count(from_unit) > 0 &&
         length_factors.count(to_unit) > 0) {
-            double from_factor = length_factors.at(from_unit);
-            double to_factor = length_factors.at(to_unit);
+        double from_factor = length_factors.at(from_unit);
+        double to_factor = length_factors.at(to_unit);
 
-            double value_in_meters = value * from_factor;
-            double result = value_in_meters / to_factor;
+        double value_in_meters = value * from_factor;
+        double result = value_in_meters / to_factor;
 
-            return result;
-        }
-    if (mass_factors.count(from_unit) > 0 &&
-        mass_factors.count(to_unit) > 0) {
-            double from_factor = mass_factors.at(from_unit);
-            double to_factor = mass_factors.at(to_unit);
+        return result;
+    }
+    if (mass_factors.count(from_unit) > 0 && mass_factors.count(to_unit) > 0) {
+        double from_factor = mass_factors.at(from_unit);
+        double to_factor = mass_factors.at(to_unit);
 
-            double value_in_kilograms = value * from_factor;
-            double result = value_in_kilograms / to_factor;
+        double value_in_kilograms = value * from_factor;
+        double result = value_in_kilograms / to_factor;
 
-            return result;
-        }
+        return result;
+    }
 
     if (volume_factors.count(from_unit) > 0 &&
         volume_factors.count(to_unit) > 0) {
-            double from_factor = volume_factors.at(from_unit);
-            double to_factor = volume_factors.at(to_unit);
+        double from_factor = volume_factors.at(from_unit);
+        double to_factor = volume_factors.at(to_unit);
 
-            double value_in_liters = value * from_factor;
-            double result = value_in_liters / to_factor;
+        double value_in_liters = value * from_factor;
+        double result = value_in_liters / to_factor;
 
-            return result;
-        }
+        return result;
+    }
 
-    if (temp_units.count(from_unit) > 0 &&
-        temp_units.count(to_unit) > 0) {
-            double temp_in_celcius = temp_units[from_unit].to_celsius(value);
-            double result = temp_units[to_unit].from_celsius(temp_in_celcius);
+    if (temp_units.count(from_unit) > 0 && temp_units.count(to_unit) > 0) {
+        double temp_in_celcius = temp_units[from_unit].to_celsius(value);
+        double result = temp_units[to_unit].from_celsius(temp_in_celcius);
 
-            return result;
-        }
-        
+        return result;
+    }
+
     // if (currency_factors.count(from_unit) > 0 &&         <-- WIP
     //     currency_factors.count(to_unit) > 0) {
     //         double from_factor = currency_factors.at(from_unit);
@@ -271,27 +279,66 @@ double convert(std::string from_unit, std::string to_unit, double value) {
 
     //         return result;
     //     }
-    
-    if ((from_is_length && !to_is_length) ||
-        (from_is_mass && !to_is_mass)     ||
-        (from_is_volume && !to_is_volume) ||
-        (from_is_temp && !to_is_temp))  {
-            throw std::runtime_error("Incompatible unit types (eg., length vs. mass)");
+
+    if ((from_is_length && !to_is_length) || (from_is_mass && !to_is_mass) ||
+        (from_is_volume && !to_is_volume) || (from_is_temp && !to_is_temp)) {
+        throw std::runtime_error(
+            "Incompatible unit types (eg., length vs. mass)");
     }
 
     else {
-        throw std::runtime_error("Unknown unit(s): from='" + from_unit + "', to='" + to_unit + "'");
+        throw std::runtime_error("Unknown unit(s): from='" + from_unit +
+                                 "', to='" + to_unit + "'");
     }
 }
 
-void print_units() // --> Start here. <--
+void print_units() {
+    std::cout << "Supported units:\n\n";
 
+    std::cout << "Length:\n  ";
+    for (const auto &[unit, factor] : length_factors) {
+        std::cout << unit << "  ";
+    }
+    std::cout << "\n\n";
 
+    std::cout << "Mass:\n  ";
+    for (const auto &[unit, factor] : mass_factors) {
+        std::cout << unit << "  ";
+    }
+    std::cout << "\n\n";
 
+    std::cout << "Volume:\n  ";
+    for (const auto &[unit, factor] : volume_factors) {
+        std::cout << unit << "  ";
+    }
+    std::cout << "\n\n";
 
-int main(int argc, char* argv[]) {
+    std::cout << "Temperature:\n  ";
+    for (const auto &[unit, funcs] : temp_units) {
+        std::cout << unit << "  ";
+    }
+    std::cout << "\n";
+}
+
+int main(int argc, char *argv[]) {
     try {
         Args args = parse_args(argc, argv);
+
+        if (args.show_help) {
+            print_usage();
+            std::cout << "For a list of units, use the -l or --list flag.\n";
+            return 0;
+        }
+
+        if (args.list_units) {
+            print_units();
+            return 0;
+        }
+
+        if (args.show_version) {
+            std::cout << "Current Version: \n\t" << _version << "\n";
+            return 0;
+        }
 
         if (args.from_unit == "" || args.to_unit == "") {
             print_usage();
@@ -299,15 +346,15 @@ int main(int argc, char* argv[]) {
         }
 
         double result = convert(args.from_unit, args.to_unit, args.value);
-    
+
         // Print out values.
         std::cout << "From: " << args.from_unit << "\n"
                   << "To: " << args.to_unit << "\n"
-                  << "Value: \033[1;32m" << result << args.to_unit << "\033[0m\n";
+                  << "Value: \033[1;32m" << result << args.to_unit
+                  << "\033[0m\n";
 
         return 0;
-    }
-    catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         std::cerr << "\033[1;31mError: \033[31m" << e.what() << "\033[0m\n";
         print_usage();
         return 1;
